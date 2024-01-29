6aafb3016b1ca00c119e95a71323e4056295ccf7
cristiansot <soluciongrafix@gmail.com>
2024-01-29 20:40:50 -0300

    Updated

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import NutritionDashboard from './components/NutritionDashboard';
+
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NutritionDashboard />
+    </div>
+  );
+}
+
+export default App;
